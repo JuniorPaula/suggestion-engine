04e9be6910045f0aed5d188a971015d528e0db75
Junior Paula <luke.junnior@icloud.com>
2025-10-26 07:08:13 -0300

    Add color text in terminal CLI

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -10,6 +10,23 @@ import (
 	"suggestion-engine/engine"
 )
 
+// clearScreen clear the terminal
+func clearScreen() {
+	fmt.Print("\003[H\033[2J")
+}
+
+// colorText mapped the color with the text
+func colorText(text, color string) string {
+	colors := map[string]string{
+		"green":  "\033[1;32m",
+		"cyan":   "\033[1;36m",
+		"yellow": "\033[1;33m",
+		"gray":   "\033[0;37m",
+		"reset":  "\033[0m",
+	}
+	return colors[color] + text + colors["reset"]
+}
+
 func main() {
 	e := engine.NewSuggestionEngine()
 	err := engine.LoadFromFile("data/searches.txt", e)
@@ -20,14 +37,15 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println(" Modo interativo do motor de sugestão")
-	fmt.Println(" Digite algo e veja as sugestões em rempo real (CRTL+C pra sair)")
-	fmt.Println("-----------------------------------------------------------")
+	clearScreen()
+	fmt.Println(colorText("🧠 Modo interativo do motor de sugestão", "cyan"))
+	fmt.Println(colorText("Digite algo e veja as sugestões em rempo real (CRTL+C pra sair)", "gray"))
+	fmt.Println(strings.Repeat("—", 55))
 
-	var in string
+	in := ""
 
 	for {
-		fmt.Print("\nBuscar: ")
+		fmt.Print(colorText("\nBuscar: ", "yellow"))
 		text, _ := reader.ReadString('\n')
 		in = strings.TrimSpace(text)
 
@@ -39,9 +57,24 @@ func main() {
 		suggestions := e.Suggest(in, 5)
 		elapsed := time.Since(start)
 
-		fmt.Printf("→ %d sugestão em %v\n", len(suggestions), elapsed)
-		for _, s := range suggestions {
-			fmt.Printf("  🞄 %-40s (score %.2f)\n", s.Word, s.Score)
+		clearScreen()
+
+		fmt.Printf("%s %s\n", colorText("🔍 Entrada:", "cyan"), colorText(in, "yellow"))
+		fmt.Printf("%s %d sugestão em %s\n", colorText("🕑 Tempo:", "cyan"), len(suggestions), colorText(fmt.Sprintf("(%v)", elapsed), "gray"))
+		fmt.Println(strings.Repeat("—", 55))
+
+		if len(suggestions) == 0 {
+			fmt.Println(colorText("Nenhuma sugestão encontrada.", "gray"))
+			continue
+		}
+
+		for i, s := range suggestions {
+			fmt.Printf("%s %-40s %s %.2f\n",
+				colorText(fmt.Sprintf("%2d.", i+1), "green"),
+				colorText(s.Word, "yellow"),
+				colorText("score", "gray"),
+				s.Score,
+			)
 		}
 	}
 }
